b2160c9d32d96e24b4e05c632bcba91738709b67
mjmj0217 <mjmj0217@naver.com>
2023-04-16 16:19:37 +0900

    feat: add safe area

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
+import { StatusBar } from "expo-status-bar";
 import * as React from "react";
-
+import { SafeAreaView } from "react-native";
 import { WebView } from "react-native-webview";
 
 export default function App() {
-  return <WebView source={{ uri: "" }} />;
+  return (
+    <>
+      <StatusBar />
+      <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+        <WebView source={{ uri: "" }} />
+      </SafeAreaView>
+    </>
+  );
 }
