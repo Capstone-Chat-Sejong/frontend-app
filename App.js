355b85fb2b90cace4ed2a072ed92c1b7729d9eca
mjmj0217 <mjmj0217@naver.com>
2023-06-02 01:52:14 +0900

    feat: schedule notification

--- App.js
+++ App.js
@@ -3,8 +3,42 @@ import * as React from "react";
 import { SafeAreaView, Platform, NativeModules, Linking } from "react-native";
 import { WebView } from "react-native-webview";
 import { KeyboardAvoidingView } from "react-native";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { WEBVIEW_URL } from "@env";
+import * as Notifications from "expo-notifications";
+import * as Device from "expo-device";
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  return token;
+}
 
 export default function App() {
   const [topAreaColor, setTopAreaColor] = useState("#F1E9E9");
@@ -32,6 +66,35 @@ export default function App() {
       : null;
   }, []);
 
+  /**notifications */
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => {
+      setExpoPushToken(token);
+    });
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
   return (
     <>
       <SafeAreaView style={{ flex: 0, backgroundColor: topAreaColor }} />
@@ -51,6 +114,21 @@ export default function App() {
             scrollEnabled={false}
             onNavigationStateChange={handleNavigationChange}
             onShouldStartLoadWithRequest={handleUrlRequest}
+            onMessage={({ nativeEvent }) => {
+              const { type, data } = JSON.parse(nativeEvent.data);
+              if (type === "SCHEDULE") {
+                Notifications.scheduleNotificationAsync({
+                  content: {
+                    title: "오늘의 일정입니다!",
+                    body: data.title,
+                    sound: true,
+                  },
+                  trigger: {
+                    seconds: data.date, // 1
+                  },
+                });
+              }
+            }}
           />
         </KeyboardAvoidingView>
       </SafeAreaView>
